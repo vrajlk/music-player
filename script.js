7a2fd719af91838b8943b1b7d1dbbdd98c0a2ab6
VRAJ LAKUM <vrajlakum2015@gmail.com>
2025-01-01 18:50:06 +0530

    added the functionalities of play and pause and songs from the library are functional and seekbar is updated with name and duration

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 console.log("Hey, it's Spotify cloned but userly modified!");
 
+let currentSong = new Audio();
+
+
 async function getData() {
     let a = await fetch("http://127.0.0.1:8080/songs");
     let response = await a.text();
@@ -23,7 +26,23 @@ async function getData() {
     return songs;
 }
 
+
+const PlayMusic = (track) => {
+//   let audio = new Audio( "/spotify%20clone/songs/" + track)
+  currentSong.src = "/spotify%20clone/songs/" + track
+  currentSong.play()
+  currenttrack.src = "images/logos/pause.svg"
+  document.querySelector(".songName").innerHTML = track
+  document.querySelector(".songDuration").innerHTML = ""
+}
+
+
+
+
 async function main() {
+
+    
+
     let songs = await getData();
     console.log("Fetched songs:", songs);
 
@@ -49,18 +68,54 @@ async function main() {
          </li>`;
     }
 
-    
-    let playButton = document.querySelector(".play")  
-    playButton.addEventListener("click", () => {
-        if (songs.length > 0) {
-            let audio = new Audio(songs[23]); // Play the first song from the array
-            audio.play()
-                .then(() => console.log("Playback started successfully"))
-                .catch((err) => console.error("Playback error:", err));
-        } else {
-            console.log("No songs found.");
-        }
+    Array.from(document.querySelector(".songslist").getElementsByTagName("li")).forEach(e => {
+        e.addEventListener("click",element =>{
+            console.log(e.querySelector(".songinfo").firstElementChild.innerHTML);
+            PlayMusic(e.querySelector(".songinfo").firstElementChild.innerHTML.trim())
+
+        })
+        
     });
+    
+    currenttrack.addEventListener("click",() => {
+      if (currentSong.paused) {
+        currentSong.play()
+        currenttrack.src = "images/logos/pause.svg"
+      }
+      else{
+        currentSong.pause()
+        currenttrack.src = "images/logos/play_pause.svg"
+      }
+    }
+    )
+
+//     let playButton = document.querySelector(".play")  
+//     playButton.addEventListener("click", () => {
+//         if (songs.length > 0) {
+//             let audio = new Audio(songs[23]); // Play the first song from the array
+//             audio.play()
+//                 .then(() => console.log("Playback started successfully"))
+//                 .catch((err) => console.error("Playback error:", err));
+//         } else {
+//             console.log("No songs found.");
+//         }
+//     });
 }
 
 main();
+
+
+// seekbarjs
+
+// Seekbar 2
+var rangetwo = document.getElementById("range-two");
+var completionBartwo = document.getElementById("completionBar-two");
+
+// Update dynamically as Seekbar 2 moves
+rangetwo.oninput = function () {
+    // Update the width of the completion bar
+    completionBartwo.style.width = this.value + '%';
+
+    // Update the background gradient
+    this.style.background = `linear-gradient(to right,rgb(255, 209, 72) ${this.value}%, rgba(211,211,211,0.4) ${this.value}%)`;
+};
