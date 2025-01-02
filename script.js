de4e5452fe8fe0f4d500c3ac0338eacc819bbcef
VRAJ LAKUM <vrajlakum2015@gmail.com>
2025-01-02 17:44:16 +0530

    time is dynamically updating so time and duration function is added and seekbar is also done seekbar is dynamically changing 2 functionalities added 1.time and duration 2.seekbar working

--- script.js
+++ script.js
@@ -1,39 +1,53 @@
 console.log("Hey, it's Spotify cloned but userly modified!");
 
 let currentSong = new Audio();
+// time converting function from seconds to minutes
+function timeconverter(seconds) {
+  if (isNaN(seconds) || seconds < 0) {
+    return "invalid time input";
+  }
+  const minutes = Math.floor(seconds / 60)
+  const remainingSeconds = Math.floor(seconds % 60)
 
+  const formattedMinutes = String(minutes).padStart(2, '0')
+  const formattedSeconds = String(remainingSeconds).padStart(2, '0')
+
+  return `${formattedMinutes}:${formattedSeconds}`
+}
+
+// async function for fetching the songs from localDirectory by enabling the node server in spotify clone folder outside the songs folder
 
 async function getData() {
-    let a = await fetch("http://127.0.0.1:8080/songs");
-    let response = await a.text();
-    let div = document.createElement("div");
-    div.innerHTML = response;
-    let as = div.getElementsByTagName("a");
-    let songs = [];
+  let a = await fetch("http://127.0.0.1:8080/songs");
+  let response = await a.text();
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let as = div.getElementsByTagName("a");
+  let songs = [];
 
-    for (let index = 0; index < as.length; index++) {
-        const element = as[index];
-        if (element.href.endsWith(".mp3")) {
-            // Correct the URL by adding 'songs/' after 'spotify clone/'
-            let correctedPath = element.href.replace(
-                "/spotify%20clone/",
-                "/spotify%20clone/songs/"
-            );
-            songs.push(correctedPath); // Add the corrected URL to the songs array
-            
-        }
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+    if (element.href.endsWith(".mp3")) {
+      // Correct the URL by adding 'songs/' after 'spotify clone/'
+      let correctedPath = element.href.replace(
+        "/spotify%20clone/",
+        "/spotify%20clone/songs/"
+      );
+      songs.push(correctedPath); // Add the corrected URL to the songs array
+
     }
-    return songs;
+  }
+  return songs;
 }
 
 
 const PlayMusic = (track) => {
-//   let audio = new Audio( "/spotify%20clone/songs/" + track)
+  //   let audio = new Audio( "/spotify%20clone/songs/" + track)
   currentSong.src = "/spotify%20clone/songs/" + track
   currentSong.play()
   currenttrack.src = "images/logos/pause.svg"
   document.querySelector(".songName").innerHTML = track
-  document.querySelector(".songDuration").innerHTML = ""
+  document.querySelector(".songDuration").innerHTML = "00:00/00:00"
 }
 
 
@@ -41,16 +55,17 @@ const PlayMusic = (track) => {
 
 async function main() {
 
-    
-
-    let songs = await getData();
-    console.log("Fetched songs:", songs);
 
 
-    let musicli = document.querySelector(".songslist").getElementsByTagName("ul")[0]
-    for (const song of songs) {
-        let songName = song.split('/').pop();
-        musicli.innerHTML += `<li> 
+  let songs = await getData();
+  console.log("Fetched songs:", songs);
+
+  // displaying all the songs fetched from the local directory in the form of list
+
+  let musicli = document.querySelector(".songslist").getElementsByTagName("ul")[0]
+  for (const song of songs) {
+    let songName = song.split('/').pop();
+    musicli.innerHTML += `<li> 
         
         
               <img class="musiclogo invert" src="images/logos/music.svg" alt="musiclogo">
@@ -66,40 +81,52 @@ async function main() {
         
         
          </li>`;
+  }
+  // library songs only name displaying and removing space
+
+  Array.from(document.querySelector(".songslist").getElementsByTagName("li")).forEach(e => {
+    e.addEventListener("click", element => {
+      console.log(e.querySelector(".songinfo").firstElementChild.innerHTML);
+      PlayMusic(e.querySelector(".songinfo").firstElementChild.innerHTML.trim())
+
+    })
+
+  });
+  // play and pause function in playbar and logo change dynamically
+
+  currenttrack.addEventListener("click", () => {
+    if (currentSong.paused) {
+      currentSong.play()
+      currenttrack.src = "images/logos/pause.svg"
     }
-
-    Array.from(document.querySelector(".songslist").getElementsByTagName("li")).forEach(e => {
-        e.addEventListener("click",element =>{
-            console.log(e.querySelector(".songinfo").firstElementChild.innerHTML);
-            PlayMusic(e.querySelector(".songinfo").firstElementChild.innerHTML.trim())
-
-        })
-        
-    });
-    
-    currenttrack.addEventListener("click",() => {
-      if (currentSong.paused) {
-        currentSong.play()
-        currenttrack.src = "images/logos/pause.svg"
-      }
-      else{
-        currentSong.pause()
-        currenttrack.src = "images/logos/play_pause.svg"
-      }
+    else {
+      currentSong.pause()
+      currenttrack.src = "images/logos/play_pause.svg"
     }
-    )
+  }
+  )
 
-//     let playButton = document.querySelector(".play")  
-//     playButton.addEventListener("click", () => {
-//         if (songs.length > 0) {
-//             let audio = new Audio(songs[23]); // Play the first song from the array
-//             audio.play()
-//                 .then(() => console.log("Playback started successfully"))
-//                 .catch((err) => console.error("Playback error:", err));
-//         } else {
-//             console.log("No songs found.");
-//         }
-//     });
+  // timeupdateliveinplaybar
+
+  // Update the time in the playbar dynamically
+  currentSong.addEventListener("timeupdate", () => {
+    console.log(currentSong.currentTime, currentSong.duration);
+
+    // Update the song duration display
+    document.querySelector(".songDuration").innerHTML = `${timeconverter(currentSong.currentTime)}/${timeconverter(currentSong.duration)}`;
+
+    // Update the seek bar position dynamically
+    if (currentSong.duration) {
+      const progressPercent = (currentSong.currentTime / currentSong.duration) * 100;
+      rangetwo.value = progressPercent;
+      completionBartwo.style.width = progressPercent + '%';
+      rangetwo.style.background = `linear-gradient(to right, rgb(255, 209, 72) ${progressPercent}%, rgba(211,211,211,0.4) ${progressPercent}%)`;
+
+    }
+  });
+
+
+
 }
 
 main();
@@ -111,11 +138,21 @@ main();
 var rangetwo = document.getElementById("range-two");
 var completionBartwo = document.getElementById("completionBar-two");
 
-// Update dynamically as Seekbar 2 moves
-rangetwo.oninput = function () {
-    // Update the width of the completion bar
-    completionBartwo.style.width = this.value + '%';
 
-    // Update the background gradient
-    this.style.background = `linear-gradient(to right,rgb(255, 209, 72) ${this.value}%, rgba(211,211,211,0.4) ${this.value}%)`;
+
+
+// Update dynamically as Seekbar moves
+rangetwo.oninput = function () {
+  const seekTime = (this.value / 100) * currentSong.duration;
+
+  // Seek the song to the new time
+  currentSong.currentTime = seekTime;
+
+  // Update the width of the completion bar
+  completionBartwo.style.width = this.value + '%';
+
+  // Update the background gradient
+  this.style.background = `linear-gradient(to right, rgb(255, 209, 72) ${this.value}%, rgba(211,211,211,0.4) ${this.value}%)`;
+
 };
+
