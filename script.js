670c834f6a2f647a874f7d44ad6f8cfe348eea02
VRAJ LAKUM <vrajlakum2015@gmail.com>
2025-01-07 13:52:19 +0530

    corrected path revoked

--- script.js
+++ script.js
@@ -32,8 +32,7 @@ async function getData(folder) {
     const element = as[index];
     if (element.href.endsWith(".mp3")) {
       // Correct the URL by adding 'songs/' after 'spotify clone/'
-      let correctedPath = `/songs/${encodeURIComponent(folder)}/${encodeURIComponent(element.href.split('/').pop())}`;
-
+      let correctedPath = `/songs/${folder}/${element.href.split('/').pop()}`;
       console.log(correctedPath);
 
       songs.push(correctedPath); // Add the corrected URL to the songs array
